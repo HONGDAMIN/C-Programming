3fc90dedae8bb6da8133ec72c579a9265324f0db
HONGDAMIN <qhqo1034@gmail.com>
2024-02-23 12:16:20 +0900

    HelloWorld

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+	int num = 24;
+	std::cout << "HelloWorld!" << std::endl;
+	std::cout << "Hello" << "World" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+
+	return 0;
+}
